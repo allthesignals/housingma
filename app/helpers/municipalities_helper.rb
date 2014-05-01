2179c3e22deb513f7c0abeda90db7682bb56fd79
Matt Cloyd <cloydster@gmail.com>
2014-04-30 21:56:58 -0400

    Add MOE helper with nil handling, and MOE class

--- app/helpers/municipalities_helper.rb
+++ app/helpers/municipalities_helper.rb
@@ -2,4 +2,9 @@ module MunicipalitiesHelper
   def trim(float)
     ("%#g" % float).gsub(/0*$/, '')
   end
+
+  def moe_or_blank(object, field_name)
+    moe = object.send "#{field_name}_me"
+    "&plusmn; #{ "%.0f" % moe }".html_safe unless moe.nil?
+  end
 end
