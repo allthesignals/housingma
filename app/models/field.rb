e39325ebdd9df9a0b83ea1062e5d7f40050a36ed
Matt Cloyd <cloydster@gmail.com>
2014-05-02 18:23:43 -0400

    Add field validation for alias

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,11 +1,12 @@
 class Field < Grouping
-  attr_accessible :alias, :name, :description
-  attr_accessor :title, :narrative
+  attr_accessible :name,  :description, :alias
+  attr_accessor   :title, :narrative
 
   belongs_to :subtopic
   delegate   :topic, to: :subtopic, allow_nil: true
 
+  validates :alias, presence: true, length: { minimum: 3, maximum: 140 }
+
   alias_method :name, :title
   alias_method :description, :narrative
-
 end
