29276ee79393de7a904707a7ca5fe8b80f35ef33
Matt Cloyd <cloydster@gmail.com>
2014-05-03 20:12:21 -0400

    Reinvigorate header. Fix Profile layout. Style download page. Hook up links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
 module ApplicationHelper
 
   def muni_page
-    @muni
+    return @muni         if @muni
+    return @profile.muni if @profile
+    return @report.muni  if @report
   end
 
   def muni_or_root_path
-    @muni ? municipality_path(@muni) : root_path
+    muni_page ? municipality_path(muni_page) : root_path
   end
 
 end
