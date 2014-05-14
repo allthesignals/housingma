0b52b32fc8acbb6e529411ec171722793605ced4
Matt Cloyd <cloydster@gmail.com>
2014-05-14 12:00:56 -0400

    Make empty narratives default to empty string, because HAML can't render nil

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140510001655) do
+ActiveRecord::Schema.define(:version => 20140514155719) do
 
   create_table "fields", :force => true do |t|
     t.string   "title"
@@ -28,28 +28,28 @@ ActiveRecord::Schema.define(:version => 20140510001655) do
 
   create_table "subtopics", :force => true do |t|
     t.string   "title"
-    t.text     "narrative"
+    t.text     "narrative",  :default => "", :null => false
     t.integer  "sort_order"
     t.integer  "topic_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
   end
 
   create_table "topic_areas", :force => true do |t|
     t.string   "title"
-    t.text     "narrative"
+    t.text     "narrative",  :default => "", :null => false
     t.integer  "sort_order"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
   end
 
   create_table "topics", :force => true do |t|
     t.string   "title"
-    t.text     "narrative"
+    t.text     "narrative",     :default => "", :null => false
     t.integer  "sort_order"
     t.integer  "topic_area_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
   end
 
 end
