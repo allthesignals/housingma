e78c9337762a9e0353f6537a7c6f19f93be39203
allthesignals <wilburnforce@gmail.com>
2014-06-05 15:58:17 -0400

    Resolved conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20140527155909) do
+ActiveRecord::Schema.define(:version => 20140514155719) do
 
   create_table "fields", :force => true do |t|
     t.string   "title"
