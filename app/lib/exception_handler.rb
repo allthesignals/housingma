a745af927a433aee72277f67868dd874579d2ff0
Matt Cloyd <cloydster@gmail.com>
2014-06-18 13:07:23 -0400

    Add some documentation / cleanup to last commit

--- app/lib/exception_handler.rb
+++ app/lib/exception_handler.rb
@@ -1,10 +1,22 @@
 module ExceptionHandler
+
+  # Include in a delegator to a class in order to
+  # rescue all errors of a certain type for every
+  # method in that class.
+
+  # Used in Calc, CalcDelegator. Also included in CommonFacade.
+
+  # Thanks to [@shime](https://github.com/shime) for the answer on StackOverflow:
+  # http://stackoverflow.com/questions/16567243/rescue-all-errors-of-a-specific-type-inside-a-module
+
   extend ActiveSupport::Concern
+
 
   included do
     include ActiveSupport::Rescuable
     rescue_from StandardError, with: :known_error
   end
+
 
   def handle_calculation_exceptions
     yield
@@ -12,11 +24,13 @@ module ExceptionHandler
     'N/A'
   end
 
+
   def handle_template_exceptions
     yield
   rescue => e
     nil
   end
+
 
   def known_error(e)
     Rails.logger.error "[ExceptionHandler] Exception #{e.class}: #{e.message}"
